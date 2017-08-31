6a18954eb83dbded9d9f1020ef41126e2ce4a27e
Diogo Barradas <diogo.barradas@outlook.com>
2017-08-31 13:03:08 +0100

    Added per configuration inspection of clustering results

--- Weka/dwt_dtw.py
+++ Weka/dwt_dtw.py
@@ -4,7 +4,7 @@ from scipy import stats
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.vq import kmeans2, vq, whiten
 from scipy.spatial.distance import euclidean, pdist, squareform
-
+from termcolor import colored
 
 def FeedDistanceMatrix():
     dtw_file = "/home/dmbb/Documents/GestureRecog/dtw/diogo_test_swipe_45cm_45.txt"
@@ -60,22 +60,60 @@ def ClusterByDTW():
 
     ward = AgglomerativeClustering(n_clusters=4, linkage='ward').fit(X)
     ward_labels = ward.labels_
-    print "Agglomerative-ward linkage clusters - "
-    print(ward_labels)
+    #print "Agglomerative-ward linkage clusters - "
+    #print(ward_labels)
 
 
     complete = AgglomerativeClustering(n_clusters=4, linkage='complete').fit(X)
     complete_labels = complete.labels_
-    print "Agglomerative-complete linkage clusters - "
-    print(complete_labels)
+    #print "Agglomerative-complete linkage clusters - "
+    #print(complete_labels)
 
 
     whitened = whiten(X)
     centroids, kmeans_labels = kmeans2(whitened, 4, minit='random')
-    print "K-means clusters - "
-    print(kmeans_labels)
+    #print "K-means clusters - "
+    #print(kmeans_labels)
 
     print "======================================================"
 
+    return {'Agglomerative_ward linkage': ward_labels, 'Agglomerative_complete linkage': complete_labels, 'K-means': kmeans_labels}
+
+
+
+def PrintResults(labels):
+    ##########################################################################################
+    #TODO: These should be merged & simplified. For instance:
+    # diogo_test_bloom_15cm_0.txt, 10
+    # diogo_test_bloom_15cm_20.txt, 11
+    #...
+
+    configuration_list = "/home/dmbb/Documents/GestureRecog/dtw/all_txt_files-20170726.txt"
+    samples_per_config = "/home/dmbb/Documents/GestureRecog/dtw/diogo_dtw_label.txt"
+
+    ##########################################################################################
+    #Retrieve number of samples from each configuration
+    n_samples = []
+    with open(samples_per_config) as f:
+        i = 5
+        for n,line in enumerate(f):
+            if(n==i):
+                s =''.join(d for d in line if d.isdigit())
+                n_samples.append(s)
+                i += 7
+
+    #We are just interested in analysing "diogo" configurations in this example
+    for k in labels:
+        print colored(k,"red")
+        with open(configuration_list) as f:
+            for n, line in enumerate(f):
+                if("diogo" in line):
+                    print line.rstrip() + "\t" + str(labels[k][:int(n_samples[n])])
+                    labels[k] = labels[k][int(n_samples[n]):]
+        print "#################################################\n"
+
+
+
 if __name__ == "__main__":
-    ClusterByDTW()
+    labels = ClusterByDTW()
+    PrintResults(labels)
